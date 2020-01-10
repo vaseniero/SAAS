8feaedb016f2ae3506f690292af104745a99277e
Vicsante Aseniero <vaseniero@gmail.com>
2020-01-10 15:10:39 +0800

    change welcome page title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>SaaS Sales and Marketing</title>
+        <title>Sales and Marketing</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
