51ba696500f29d3458e3f5029b77c84a9f86ca9e
Vicsante Aseniero <vaseniero@gmail.com>
2020-01-11 15:37:29 +0800

    changes in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sales & Marketing
                 </div>
 
                 <div class="links">
